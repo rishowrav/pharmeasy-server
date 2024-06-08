35b8b8cb4ed25796ef9a33a9fb41ea428bc43724
rishowrav <rishowrav@gmail.com>
2024-06-08 23:14:03 +0600

    jwt setup done

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const cookieParser = require("cookie-parser");
+
+// middleware
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+
+// verify token middleware
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  //   console.log("token in the middleware", token);
+
+  if (!token) {
+    return res
+      .status(401)
+      .send({ message: "unauthorized access token pai nai re vai" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ message: "unauthorized access token valid na re vai" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
 const uri = `mongodb+srv://${process.env.USER_name}:${process.env.USER_pass}@cluster0.sp25joa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -17,8 +51,27 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    // clearing Token / LogOut token
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -26,7 +79,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
